14b15ac6a964a84f9a862ef88cf0c1b18272d0b9
joaopedroaquino <aqerhack@gmail.com>
2023-01-17 21:50:04 -0300

    create bootstrap func

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,13 @@
+import { ApolloServer } from "apollo-server";
 import "reflect-metadata";
+
+async function bootstrap(){
+    const server = new ApolloServer({})
+
+    server.listen().then(({ url }) => {
+        console.log(`Server running on ${url}`);
+    })
+
+}
+
+bootstrap()
