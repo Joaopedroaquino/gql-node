ef138eb7dc18d3642995252355590af3862a0bbe
joaopedroaquino <aqerhack@gmail.com>
2023-01-19 21:05:17 -0300

    init querys

--- src/resolvers/appointments-resolver.ts
+++ src/resolvers/appointments-resolver.ts
@@ -1,4 +1,9 @@
-import { Resolver } from "type-graphql";
+import { Query, Resolver } from "type-graphql";
 
 @Resolver()
-export class AppointmentsResolver {}
+export class AppointmentsResolver {
+    @Query()
+    async helloWorld(){
+        return 'Hello World'
+    }
+}
