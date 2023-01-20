5244e382d6b1e6f0dadb1030158aa84291d24002
joaopedroaquino <aqerhack@gmail.com>
2023-01-19 21:16:34 -0300

    add resolver

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,16 @@
 import { ApolloServer } from "apollo-server";
+import path from 'node:path'
 import "reflect-metadata";
+import { buildSchema } from "type-graphql";
+import { AppointmentsResolver } from "./resolvers/appointments-resolver";
 
 async function bootstrap(){
+    const schema = await buildSchema({
+        resolvers: [
+            AppointmentsResolver,
+        ],
+        emitSchemaFile: path.resolve(__dirname, 'schema.gql')
+    })
     const server = new ApolloServer({})
 
     server.listen().then(({ url }) => {
